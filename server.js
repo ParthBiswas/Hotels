bc16f88d4c560556c34453afeb7d84c43de7fef0
Parth Biswas <Parthbiswas@outlook.com>
2024-06-08 18:07:46 +0530

    Shift Mngo Connection

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db= require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const PORT= process.env.PORT || 3001;
 
 app.get('/', function (req, res) {
   res.send('Welcome to Our Server')
@@ -16,6 +19,8 @@ app.use('/menu',menuRouter)
 const personRouter = require('./routers/personRouter');
 app.use('/person',personRouter)
 
-app.listen(3000, () => {
-  console.log('Listing on port 3000');
+
+
+app.listen(PORT, () => {
+  console.log('Listing on port 3001');
 })
