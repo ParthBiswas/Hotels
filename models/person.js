33b1e9ec4d77078db48e2a51d8733c6c1eb72523
Parth Biswas <Parthbiswas@outlook.com>
2024-06-10 23:26:53 +0530

    new add

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 //Define the Person schema
 const personSchema = new mongoose.Schema({
@@ -31,8 +32,37 @@ const personSchema = new mongoose.Schema({
     salary:{
         type: Number,
         required : true
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
     }
 });
+
+prosonSchema.pre('save', async function(next){
+    const person = this;
+
+    //Hash the Password only if modified (or is new)
+    if (!person.isModified('password')) return next();
+
+    try{
+        // hash password genration
+        const salt = await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword = await bcrypt.hashPassword(person.password, salt);
+
+        //override the plain password with hashed one
+        person.password = hashedPassword;
+        next()
+    }catch(err){
+        
+    }
+})
 
  //create Person
 
--- server.js
+++ server.js
@@ -2,24 +2,31 @@ const express = require('express')
 const app = express();
 const db= require('./db');
 require('dotenv').config();
+const passport = require('./auth');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
 const PORT= process.env.PORT || 3001;
 
-app.get('/', function (req, res) {
+const menuRouter = require('./routers/menuRouter');
+const personRouter = require('./routers/personRouter');
+
+// Middleware function
+const logRequest = (req, res, next) =>{
+  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.orignalUrl}`);
+  next(); 
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const passAuth = passport.authenticate('local', {session: false});
+
+app.get('/',passAuth,function (req, res) {
   res.send('Welcome to Our Server')
 })
 
-const menuRouter = require('./routers/menuRouter');
 app.use('/menu',menuRouter)
-
-
-const personRouter = require('./routers/personRouter');
-app.use('/person',personRouter)
-
-
+app.use('/person',passAuth,personRouter)
 
 app.listen(PORT, () => {
   console.log('Listing on port 3001');
